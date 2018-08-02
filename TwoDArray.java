a0c1a5c1d89e4641329f5a1a65b8620aabd693e1
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-02 17:06:02 +0600

    2D array input output operation.

--- TwoDArray.java
+++ TwoDArray.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package basicjava;
 
-/**
- *
- * @author Asus
- */
+import java.util.Scanner;
+
+
 public class TwoDArray {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[][] arr = new int[3][3];
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = input.nextInt();
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr[i][j]+" ");
+            } 
+            System.out.println();
+        }
+    }
 }
