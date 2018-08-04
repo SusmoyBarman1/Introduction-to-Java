6762612f3fbd231efb33eacb2799e801b77f71b8
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-04 16:37:08 +0600

    Add files via upload

---
+++ Random_Number.java
@@ -0,0 +1,10 @@
+
+package basicjava;
+
+public class Random_Number {
+    public static void main(String[] args) {
+        
+        int randomNumber = (int) (Math.random() * 10);
+        System.out.println(randomNumber);
+    }
+}
