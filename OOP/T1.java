8b47c0df2ee2ebba683c30134dc1b5e6a1233a69
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-06 21:06:49 +0600

    Add files via upload

---
+++ OOP/T1.java
@@ -0,0 +1,16 @@
+
+package oop;
+
+public class T1 {
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher();
+        teacher.gender = "male";
+        teacher.name = "Susmoy Barman";
+        teacher.phone = 1774668821;
+        
+        System.out.println(teacher.name);
+        System.out.println(teacher.gender);
+        System.out.println(teacher.phone);
+        
+    }
+}
