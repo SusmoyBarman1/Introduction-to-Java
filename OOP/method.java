8cbdf2ad8d0a92093e0b3d47bb2efed5795540c2
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-08 15:21:32 +0600

    using method or function

---
+++ OOP/method.java
@@ -0,0 +1,14 @@
+
+
+public class method {
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher();
+        teacher.setInformation("Susmoy Barman","male", 1774668821);
+        teacher.displayInformation();
+        System.out.println();
+        
+        Teacher teacher1 = new Teacher();
+        teacher1.setInformation("Rabindranath Thakur", "male", 121);
+        teacher1.displayInformation();
+    }
+}
