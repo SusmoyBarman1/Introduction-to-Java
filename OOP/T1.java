70d61d2d66569047785857516025ff457aa47e28
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-08 15:10:30 +0600

    Add files via upload

--- OOP/T1.java
+++ OOP/T1.java
@@ -1,5 +1,4 @@
 
-package oop;
 
 public class T1 {
     public static void main(String[] args) {
@@ -7,10 +6,13 @@ public class T1 {
         teacher.gender = "male";
         teacher.name = "Susmoy Barman";
         teacher.phone = 1774668821;
+        teacher.displayInformation();
+        System.out.println();
         
-        System.out.println(teacher.name);
-        System.out.println(teacher.gender);
-        System.out.println(teacher.phone);
-        
+        Teacher teacher1 = new Teacher();
+        teacher1.gender = "male";
+        teacher1.name = "Rabindronath Thakur";
+        teacher1.phone = 121;
+        teacher1.displayInformation();
     }
 }
--- OOP/Teacher.java
+++ OOP/Teacher.java
@@ -1,7 +1,12 @@
 
-package oop;
-
 public class Teacher {
     String name,gender;
     long phone;
+
+    void displayInformation()
+    {
+        System.out.println(name);
+        System.out.println(gender);
+        System.out.println(phone);
+    }
 }
