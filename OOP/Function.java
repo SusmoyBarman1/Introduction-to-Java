4aacaef65cc44ea18c572509fbfcbb80526836f1
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-09 17:03:05 +0600

    Add files via upload

---
+++ OOP/Function.java
@@ -0,0 +1,19 @@
+public class Function
+{
+    int square(int a)
+    {
+        return a*a;
+    }
+    int multiplication(int a,int b)
+    {
+        return a*b;
+    }
+    int multiplication(int a,int b,int c)
+    {
+        return a*b*c;
+    }
+    double multiplication(double a,double b)
+    {
+        return a*b;
+    }
+}
