33850c407e333350adcf30f03200456c2e958a3c
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-09 17:02:04 +0600

    Add files via upload

---
+++ OOP/Static_Variable.java
@@ -0,0 +1,8 @@
+public class Static_Variable
+{
+    // To use instance variable, we need to call the variable with object of the class.
+    // To use static variable, we need to call the variable with the class name.
+
+    String name = "Susmoy Barman";
+    static String varsity = "Rajshahi University";
+}
