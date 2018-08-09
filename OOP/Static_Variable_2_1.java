fcb5c0ec2acd163c867286658f88b16d2fd2d95c
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-09 17:03:35 +0600

    Add files via upload

---
+++ OOP/Static_Variable_2_1.java
@@ -0,0 +1,19 @@
+public class Static_Variable_2_1
+{
+    public static void main(String[] args)
+    {
+        /*Static_Variable_2 ob1 = new Static_Variable_2();
+        ob1.displayValue();
+        Static_Variable_2 ob2 = new Static_Variable_2();
+        ob2.displayValue();
+        Static_Variable_2 ob3 = new Static_Variable_2();
+        ob3.displayValue(); */
+
+        Static_Variable_2 ob4 = new Static_Variable_2();
+        ob4.displayValue1();
+        Static_Variable_2 ob5 = new Static_Variable_2();
+        ob5.displayValue1();
+        Static_Variable_2 ob6 = new Static_Variable_2();
+        ob6.displayValue1();
+    }
+}
