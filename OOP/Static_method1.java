434fa2698a88de94e48370fdf4e9414f325c799d
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-09 17:23:26 +0600

    Add files via upload

---
+++ OOP/Static_method1.java
@@ -0,0 +1,11 @@
+public class Static_method1
+{
+    public static void main(String[] args)
+    {
+        Static_Method ob = new Static_Method();
+        
+        ob.display1();
+
+        Static_Method.display2();
+    }
+}
