04887ee1fa5e6164750d203f5a1253b9b80ccc20
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-09 17:12:06 +0600

    Add files via upload

---
+++ Static_Variable1.java
@@ -0,0 +1,9 @@
+public class Static_Variable1
+{
+    public static void main(String[] args)
+    {
+        Static_Variable ob = new Static_Variable();
+        System.out.println("\nName: "+ob.name);
+        System.out.println("\nVarsity: "+Static_Variable.varsity+"\n");
+    }
+}
