e76e81db875694b7fd2e7dcd33d6381e5ad059d3
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-09 17:23:02 +0600

    Add files via upload

---
+++ OOP/Static_Method.java
@@ -0,0 +1,12 @@
+public class Static_Method
+{
+    void display1()
+    {
+        System.out.println("I am not static.\n");
+    }
+
+    static void display2()
+    {
+        System.out.println("I am Static.\n");
+    }
+}
