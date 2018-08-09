5921e9e561367d36ce7d9721a2e64d31ca55e424
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-09 17:03:59 +0600

    Add files via upload

---
+++ OOP/StaticVariable1.java
@@ -0,0 +1,13 @@
+public class StaticVariable1
+{
+    public static void main(String[] args)
+    {
+        StaticVariable ob = new StaticVariable("Susmoy Barman",1610576113,"RU");
+        StaticVariable ob1 = new StaticVariable("Fozlay Rabbi",1644554455,"JU");
+
+        ob.displayInformation();
+        System.out.println("\n");
+        ob1.displayInformation();
+        System.out.println("\n");
+    }
+}
