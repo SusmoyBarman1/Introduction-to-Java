a3c38b0e32ead5b72f2ef55431d1973dd4b9e2cc
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-09 17:02:28 +0600

    Add files via upload

---
+++ OOP/Static_Variable_2.java
@@ -0,0 +1,23 @@
+public class Static_Variable_2
+{
+    /*int count = 0; // Non static variable
+    
+    Static_Variable_2()
+    {
+        count++;
+    }
+    void displayValue()
+    {
+        System.out.println("Count: "+count);
+    }*/
+
+    static int count1 = 0;
+    Static_Variable_2()
+    {
+        count1++;
+    }
+    void displayValue1()
+    {
+        System.out.println("Count1: "+count1);
+    } 
+}
