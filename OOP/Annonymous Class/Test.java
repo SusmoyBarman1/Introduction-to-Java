d0d759540917b0374fe438916839581505b1340d
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-30 20:38:16 +0600

    Add files via upload

---
+++ OOP/Annonymous Class/Test.java
@@ -0,0 +1,14 @@
+public class Test
+{
+    public static void main(String[] args) {
+        Person p = new Person()
+        {
+            void display()
+            {
+                System.out.println("Test class");
+            }
+        };
+
+        p.display();
+    }
+}
