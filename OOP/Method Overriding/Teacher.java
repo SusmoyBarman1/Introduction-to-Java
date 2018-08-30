93b7f9e3e85a86eaef4397132e7a8e7bf784b146
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-30 20:46:35 +0600

    Add files via upload

---
+++ OOP/Method Overriding/Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher extends Person
+{
+    String qualification;
+
+    void displayInfo()
+    {
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Qualification: "+qualification);
+    }
+}
---
+++ OOP/Method Overriding/Test.java
@@ -0,0 +1,13 @@
+public class Test
+{
+    public static void main(String[] args)
+    {
+        Teacher t1 = new Teacher();
+
+        t1.name = "Omar Faruk";
+        t1.age = 31;
+        t1.qualification = "BSc in CSE,RU";
+
+        t1.displayInfo();
+    }
+}
