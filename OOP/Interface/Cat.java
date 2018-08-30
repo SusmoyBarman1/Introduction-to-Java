70500c46e0fb8e64fceacda5cac2ce239ed16dd8
Susmoy Barman <susmoy11223300@gmail.com>
2018-08-30 20:48:34 +0600

    Add files via upload

---
+++ OOP/Interface/Cat.java
@@ -0,0 +1,8 @@
+public class Cat implements Animal
+{
+    // we can not make a variable of an interface.
+    public void eat()
+    {
+        System.out.println("\nCats eat Milk!\n");
+    }
+}
---
+++ OOP/Interface/Dog.java
@@ -0,0 +1,8 @@
+public class Dog implements Animal
+{
+    // we can not make a variable of an interface.
+    public void eat()
+    {
+        System.out.println("\nDogs eat Banana!\n");
+    }
+}
